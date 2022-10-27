6b79b594de80a71bc15821c3982ee033d6e43552
Jaehyun An <lvet0330@gmail.com>
2022-10-27 14:40:24 +0900

    # 1545
    끝

---
+++ src/d1/D1_1545.java
@@ -0,0 +1,17 @@
+package d1;
+
+import java.util.Scanner;
+
+public class D1_1545 {
+    public static void main(String args[]) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T = sc.nextInt();
+
+        for (int tc = 0; tc <= T; tc++) {
+            System.out.print(T-tc+" ");
+
+        }
+    }
+}
