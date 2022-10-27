603962a708fef7242deb180ddce564c336a619f3
Jaehyun An <lvet0330@gmail.com>
2022-10-27 14:15:42 +0900

    #2043

---
+++ src/d1/D1_2043_scanner.java
@@ -0,0 +1,17 @@
+package d1;
+
+import java.util.Scanner;
+
+public class D1_2043_scanner {
+    public static void main(String args[]) throws Exception
+    {
+
+        Scanner sc = new Scanner(System.in);
+        int T,J;
+        T=sc.nextInt();
+        J=sc.nextInt();
+
+        System.out.println(T-J+1);
+    }
+}
+
