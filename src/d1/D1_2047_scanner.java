9a3147fc72bc5b8b00fcb7d24d10130f93262e82
Jaehyun An <lvet0330@gmail.com>
2022-10-27 14:04:55 +0900

    # 2047 스캐너 사용
    신문 헤드라인

---
+++ src/d1/D1_2047_scanner.java
@@ -0,0 +1,15 @@
+package d1;
+import java.util.Scanner;
+
+class D1_2047_scanner
+{
+    public static void main(String args[]) throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        String str = "";
+        String T;
+        T=sc.nextLine();
+        str = T.toUpperCase();
+        System.out.println(str);
+    }
+}
