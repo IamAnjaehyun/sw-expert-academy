265eb6e35bd206c0eb9be3ed150ebf9f6765391d
Jaehyun An <lvet0330@gmail.com>
2022-10-27 14:04:48 +0900

    #2046 스캐너 사용
    스탬프 찍기

---
+++ src/d1/D1_2046_scanner.java
@@ -0,0 +1,16 @@
+package d1;
+
+import java.util.Scanner;
+
+public class D1_2046_scanner {
+    public static void main(String args[]) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T = sc.nextInt();
+
+        for (int tc = 1; tc <= T; tc++) {
+            System.out.print("#");
+        }
+    }
+}
+
