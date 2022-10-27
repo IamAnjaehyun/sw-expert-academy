db39a17be23f671ed5f0657e362d1d1a54ea3355
Jaehyun An <lvet0330@gmail.com>
2022-10-27 14:26:57 +0900

    #1938
    사칙연산

---
+++ src/d1/D1_1938.java
@@ -0,0 +1,20 @@
+package d1;
+
+
+import java.util.Scanner;
+
+public class D1_1938 {
+    public static void main(String[] args) throws Exception {
+        {
+            Scanner sc = new Scanner(System.in);
+            int T, C;
+            T = sc.nextInt();
+            C = sc.nextInt();
+
+            System.out.println((T + C));
+            System.out.println((T - C));
+            System.out.println((T * C));
+            System.out.println((T / C));
+        }
+    }
+}
