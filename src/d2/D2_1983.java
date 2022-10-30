c8c2f2b6ec6f8225589a62fb74d2ba6c21703e37
Jaehyun An <lvet0330@gmail.com>
2022-10-30 23:38:26 +0900

    # 1983 진행중,,
    double형이 오류인 줄 알고 int형으로 풀어도 안된다 뭐징..

--- src/d2/D2_1983.java
+++ src/d2/D2_1983.java
@@ -1,12 +1,10 @@
 package d2;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Scanner;
+import java.util.*;
 
 public class D2_1983 {
+    static String score[] = {"A+", "A0", "A-", "B+", "B0", "B-", "C+", "C0", "C-", "D0"};
     public static void main(String args[]) throws Exception {
-        String score[] = {"A+", "A0", "A-", "B+", "B0", "B-", "C+", "C0", "C-", "D0"};
         Scanner sc = new Scanner(System.in);
         int T;
         T = sc.nextInt();
@@ -14,22 +12,29 @@ public class D2_1983 {
         for (int tc = 0; tc < T; tc++) {
             int std = sc.nextInt(); //학생
             int stdNum = sc.nextInt(); //학생번호
-            double stdScore = 0.0;
-            double result[] = new double[std];
 
-            for (int i = 0; i < 3; i++) {
-                double sc1 = sc.nextInt() * 0.35;
-                double sc2 = sc.nextInt() * 0.45;
-                double sc3 = sc.nextInt() * 0.20;
-                double sum = sc1 + sc2 + sc3;
+            int stdScore = 0;
+            int result[] = new int[std];
+
+            for (int i = 0; i < std; i++) {
+                int sc1 = sc.nextInt() * 35;
+                int sc2 = sc.nextInt() * 45;
+                int sc3 = sc.nextInt() * 20;
+                int sum = sc1 + sc2 + sc3;
                 result[i] = sum;
-                if (i + 1 == std) {
-                    stdScore = sum;
-                }
-                Arrays.sort(result, Collections.reverseOrder());
-                System.out.println(result[tc]);
+                if (i + 1 == stdNum) stdScore = sum;
             }
-            System.out.println();
+            Arrays.sort(result, Collections.reverseOrder());
+
+            int index = 0;
+            for (int i = 0; i < result.length; i++) {
+                if (result[i] == stdScore) index = i;
+            }
+            index = index / (std / 10);
+
+            System.out.printf("#%d %s\n", tc, score[index]);
         }
+
     }
 }
+
