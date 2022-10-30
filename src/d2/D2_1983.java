746a43e6151c1c8a86e9e89a4bbc80c85224e2db
Jaehyun An <lvet0330@gmail.com>
2022-10-30 23:26:29 +0900

    # 1983 진행중,,
    double은 Arrays.sort가 안된다.. 다시해보자

--- src/d2/D2_1983.java
+++ src/d2/D2_1983.java
@@ -1,29 +1,33 @@
 package d2;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class D2_1983 {
     public static void main(String args[]) throws Exception {
+        String score[] = {"A+", "A0", "A-", "B+", "B0", "B-", "C+", "C0", "C-", "D0"};
         Scanner sc = new Scanner(System.in);
         int T;
         T = sc.nextInt();
-        int std = sc.nextInt();
-        int num = sc.nextInt();
-        for (int tc = 0; tc < T; tc++) {
-            for (int o = 0; o < std; o++) {
-                double rank[] = new double[o];
-                double sum = 0;
-                for (int i = 0; i < 3; i++) {
-                    double a = sc.nextInt() * 0.35;
-                    double b = sc.nextInt() * 0.45;
-                    double c = sc.nextInt() * 0.20;
-                    sum = a + b + c;
-                    rank[o] = sum;
 
+        for (int tc = 0; tc < T; tc++) {
+            int std = sc.nextInt(); //학생
+            int stdNum = sc.nextInt(); //학생번호
+            double stdScore = 0.0;
+            double result[] = new double[std];
+
+            for (int i = 0; i < 3; i++) {
+                double sc1 = sc.nextInt() * 0.35;
+                double sc2 = sc.nextInt() * 0.45;
+                double sc3 = sc.nextInt() * 0.20;
+                double sum = sc1 + sc2 + sc3;
+                result[i] = sum;
+                if (i + 1 == std) {
+                    stdScore = sum;
                 }
-                System.out.println("rank" + o + " = " + rank[o]);
-                Arrays.sort(rank);
+                Arrays.sort(result, Collections.reverseOrder());
+                System.out.println(result[tc]);
             }
             System.out.println();
         }
